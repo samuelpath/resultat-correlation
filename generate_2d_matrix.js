8108afc370bd555bac55b3dbdc6e112daac15c3b
Samuel Path <samuel.path@gmail.com>
2022-05-15 21:06:51 +0200

    fix

--- generate_2d_matrix.js
+++ generate_2d_matrix.js
@@ -19,109 +19,94 @@ function occurrences(string, subString, allowOverlapping) {
 }
 
 var res =
-`"Au centre","Plutôt verticale"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Très à gauche","Plutôt horizontale"
-"Très à gauche","Horizontale, certainement"
-"Plutôt à gauche","Plutôt verticale"
-"Au centre","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à gauche","Horizontale, certainement"
-"Très à gauche","Peu d'importance"
-"Très à gauche","Plutôt horizontale"
-"Très à droite","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Très à gauche","Plutôt verticale"
-"Très à gauche","Plutôt verticale"
-"Très à gauche","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à gauche","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Peu d'importance"
-"Plutôt à droite","Horizontale, certainement"
-"Plutôt à droite","Verticale, certainement"
-"Au centre","Plutôt horizontale"
-"Au centre","Plutôt horizontale"
-"Très à gauche","Horizontale, certainement"
-"Plutôt à gauche","Peu d'importance"
-"Plutôt à gauche","Verticale, certainement"
-"Très à gauche","Verticale, certainement"
-"Très à droite","Horizontale, certainement"
-"Très à droite","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Au centre","Horizontale, certainement"
-"Plutôt à gauche","Horizontale, certainement"
-"Très à gauche","Horizontale, certainement"
-"Plutôt à droite","Horizontale, certainement"
-"Plutôt à droite","Plutôt verticale"
-"Plutôt à gauche","Peu d'importance"
-"Au centre","Plutôt horizontale"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à gauche","Plutôt horizontale"
-"Au centre","Plutôt horizontale"
-"Plutôt à gauche","Peu d'importance"
-"Au centre","Horizontale, certainement"
-"Plutôt à droite","Plutôt horizontale"
-"Plutôt à droite","Peu d'importance"
-"Au centre","Horizontale, certainement"
-"Plutôt à droite","Horizontale, certainement"
-"Au centre","Plutôt horizontale"
-"Très à gauche","Horizontale, certainement"
-"Plutôt à gauche","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Au centre","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à gauche","Plutôt horizontale"
-"Au centre","Peu d'importance"
-"Très à gauche","Plutôt horizontale"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Horizontale, certainement"
-"Plutôt à gauche","Plutôt verticale"
-"Plutôt à gauche","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Peu d'importance"
-"Plutôt à gauche","Horizontale, certainement"
-"Plutôt à gauche","Plutôt horizontale"
-"Plutôt à droite","Horizontale, certainement"
-"Plutôt à droite","Peu d'importance"
-"Plutôt à droite","Plutôt verticale"
-"Plutôt à gauche","Plutôt horizontale"
-"Au centre","Plutôt horizontale"
-"Très à gauche","Plutôt horizontale"
-"Plutôt à gauche","Peu d'importance"
-"Très à gauche","Horizontale, certainement"`;
-
-let resNum = res
-	.replaceAll('"','')
-  .replaceAll("Très à gauche",-2)
-	.replaceAll("Plutôt à gauche",-1)
-	.replaceAll("Au centre",0)
-  .replaceAll("Plutôt à droite",1)
-	.replaceAll("Très à droite",2)
-	.replaceAll("Horizontale, certainement",-2)
-	.replaceAll("Plutôt horizontale",-1)
-	.replaceAll("Peu d'importance",0)
-	.replaceAll("Plutôt verticale",1)
-	.replaceAll("Verticale, certainement",2);
-
-let nbInputs = resNum.split(/\r\n|\r|\n/).length;
-
+`Au centre|Plutôt verticale
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Très à gauche|Plutôt horizontale
+Très à gauche|Horizontale, certainement
+Plutôt à gauche|Plutôt verticale
+Au centre|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Plutôt à gauche|Horizontale, certainement
+Très à gauche|Peu d'importance
+Très à gauche|Plutôt horizontale
+Très à droite|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Très à gauche|Plutôt verticale
+Très à gauche|Plutôt verticale
+Très à gauche|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Plutôt à gauche|Plutôt horizontale
+Plutôt à gauche|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Peu d'importance
+Plutôt à droite|Horizontale, certainement
+Plutôt à droite|Verticale, certainement
+Au centre|Plutôt horizontale
+Au centre|Plutôt horizontale
+Très à gauche|Horizontale, certainement
+Plutôt à gauche|Peu d'importance
+Plutôt à gauche|Verticale, certainement
+Très à gauche|Verticale, certainement
+Très à droite|Horizontale, certainement
+Très à droite|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Au centre|Horizontale, certainement
+Plutôt à gauche|Horizontale, certainement
+Très à gauche|Horizontale, certainement
+Plutôt à droite|Horizontale, certainement
+Plutôt à droite|Plutôt verticale
+Plutôt à gauche|Peu d'importance
+Au centre|Plutôt horizontale
+Plutôt à droite|Plutôt horizontale
+Plutôt à gauche|Plutôt horizontale
+Au centre|Plutôt horizontale
+Plutôt à gauche|Peu d'importance
+Au centre|Horizontale, certainement
+Plutôt à droite|Plutôt horizontale
+Plutôt à droite|Peu d'importance
+Au centre|Horizontale, certainement
+Plutôt à droite|Horizontale, certainement
+Au centre|Plutôt horizontale
+Très à gauche|Horizontale, certainement
+Plutôt à gauche|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Au centre|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Plutôt à gauche|Plutôt horizontale
+Au centre|Peu d'importance
+Très à gauche|Plutôt horizontale
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Horizontale, certainement
+Plutôt à gauche|Plutôt verticale
+Plutôt à gauche|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Peu d'importance
+Plutôt à gauche|Horizontale, certainement
+Plutôt à gauche|Plutôt horizontale
+Plutôt à droite|Horizontale, certainement
+Plutôt à droite|Peu d'importance
+Plutôt à droite|Plutôt verticale
+Plutôt à gauche|Plutôt horizontale
+Au centre|Plutôt horizontale
+Très à gauche|Plutôt horizontale
+Plutôt à gauche|Peu d'importance
+Très à gauche|Horizontale, certainement`;
+            
 let matrix = Array(5).fill().map(()=>Array(5).fill());
 
-for(let politicsIdx = 0; politicsIdx < 5; politicsIdx++) {
-  for(let structureIdx = 0; structureIdx < 5; structureIdx++) {
-    let politicsX = politicsIdx - 2;
-    let structureY = structureIdx - 2;
-    count = occurrences(resNum, `${politicsX},${structureY}`);
-    percent = (count / nbInputs) * 100;
-    matrix[politicsIdx][structureIdx] = count;
-  }
-}
+const structures = ["Horizontale, certainement","Plutôt horizontale","Peu d'importance","Plutôt verticale","Verticale, certainement"];
+const politics = ["Très à gauche","Plutôt à gauche","Au centre","Plutôt à droite","Très à droite"];
+
+structures.forEach((structure, structureIdx) => {
+  politics.forEach((politic, politicIdx) => {
+    count = occurrences(res, `${politic}|${structure}`);
+    matrix[Math.abs(structureIdx -4)][politicIdx] = count;
+  })
+});
 
 matrix
--- generate_heatmap.py
+++ generate_heatmap.py
@@ -1,18 +1,21 @@
 import plotly.express as px
 
+# -2,2 | -1,2 | 0,2 | 1,2 | 2,2
+# -2,1 | -1,1…
+
 data = [
-  [ 5, 5, 1, 2, 1 ],
-  [ 6, 14, 4, 2, 1 ],
-  [ 4, 7, 1, 1, 0 ],
-  [ 11, 22, 8, 4, 2 ],
-  [ 8, 5, 1, 2, 1 ]
+  [ 1, 1, 0, 1, 0 ],
+  [ 2, 2, 1, 2, 0 ],
+  [ 1, 4, 1, 4, 0 ],
+  [ 5, 14, 7, 8, 0 ],
+  [ 5, 6, 4, 5, 3 ]
 ]
 
 fig = px.imshow(
      data,
      labels=dict(x="Orientation politique", y="Structure d'organisation", color="Nombre de participants"),
      x=['Très à gauche', 'Plutôt à gauche', 'Au centre', 'Plutôt à droite', 'Très à droite'],
-     y=['Horizontale, certainement', 'Plutôt horizontale', 'Peu d\'importance', 'Plutôt verticale', 'Verticale, certainement'],
+     y=['Verticale, certainement', 'Plutôt vertical', 'Peu d\'importance', 'Plutôt horizontale', 'Horizontale, certainement'],
      text_auto=True
 )
 fig.write_html("index.html")
